89bde89e08991ba5c5b2221034436364f4380ee0
Ayush Agarwal <thisisayushaa@gmail.com>
2017-04-01 12:50:45 +0530

    Rededigned UI Update 1

--- js/data.js
+++ js/data.js
@@ -28,9 +28,9 @@ var rankers = {name1:"Sample", ppicUrl1: "images/alumni/1.jpg",rank1:"0000", nam
 var services = [];
 var events = [];
 var webinars = [];
+var openSource = [];
+var eventsP = [];
 var team = [];
-var pics = [];
-var resource = [];
 var footerContactLinks = [];
 /** Services List **/
 
@@ -64,7 +64,6 @@ services[6] = "Already know something? Teach it to others! That's how we work!";
  */
 events[0] = {title:"No Upcoming Event",cta:"https://docs.google.com/forms/d/e/1FAIpQLScpSM7nisQQKlBB5oYdkeo2cMV6I0b__F_KowLgbUJePyyLZg/viewform?usp=sf_link",ctaText:"Host an Event!",date:"N/A",startTime:"N/A", endTime:"N/A",location:"Amity University",eventOD:1, description:'<strong>Want to give a talk or coduct a workshop?</strong> <br> Just click on the link below and fill up the form :) to conduct a workshop or a webinar under ALiAS and spread the knowledge! <br><strong><em>Let Knowledge be open source!</em></strong>', cta2enable:0};
 /** End Events List **/
-
 /** Webinars List  
 	title: Title of webinar
 	videoId: https://youtube.com/watch?v=video_id
@@ -75,40 +74,37 @@ webinars[0] = {title: "Webinar: Introduction to Linux", videoId: "CTO-8BnzS_s", 
 webinars[1] = {title: "Webinar: Introduction to Command Line Interface", videoId:"gmi_YM_N2L4", description: "This webinar would be about basics of using Command Line Interface in Linux. We'll be covering basics of command lines/ principles/ every 'Must Know' things about Linux. <br/><strong>Speakers:</strong> <br/><strong>Rohit Goyal:<strong> <a href = \"https://about.me/rohitgoyal18\">about.me/rohit</a> "};
 webinars[2]= {title: "Webinar: Open Source Softwares", videoId: "JcYABQrAkaU", description: "Learn the basics of Open Source and Open Source Software in this live interactive session.<br/><strong>Speakers:</strong> <br/><strong>Amit Kumar:<strong> <a href = \"http://iamit.in\">http://iamit.in</a> <br/><stong>Sanyam Khurana:</strong> <a href=\"http://www.sanyamkhurana.com\">sanyamkhurana.com</a>"};
 webinars[3] = {title:"'Wordpress 101' by Samriddha Prajapati", videoId: "fjxUa7HX0NY", description:"Learn WordPress in this interactive webinar by <strong>Samriddha Prajapati</strong>. <br/><em>Hi, I’m Samriddha Prajapati and I specialize in Computer Software. I’m passionate about what I do, and I love to help people. Nothing is more fulfilling than being part of a team with similar interests, and an organization that values its employees.</em>"};
-/** Picture Box
-	url: Link of the cover photo on google photos (For perfect size)
-	text: Name of the event
-	date: Date of the event
-	cta: call to action (Link of the photos album) 
-	pics[]= {url:"", text:"", date:"", cta:""};
-**/
-pics[0]= {url:"https://lh3.googleusercontent.com/ToOSptmfFXY6qEXRlK7wgtUxBMYEjnt39pSBr_AxVXjyPPmpDa3pnnjdjAxQM3CDAk79GJrC1c0Y3CFTs9a8tIbEN9X-SGMywTZ7R001aOFwak9BzbC-dTb28oU3TrzWf2a9CynzbiZ0nhM4n4jqlw4IUWVLdLDKvayecAqElDxoON-hX-858oe6GjIF6QI4K-h7ETLiMi_SazzlMqoLuh0DiGo7LKFR1T2znC8Msf8GHELrqUZl3gNb_K54Ri9NYBl33S23Fc_4xMCVMBB-mogkCgTkt1qIcMaqg5e-YF-1O-lPLNSkC9qveEApcwzjQdXQflUVhHTTitfRLH0X8DL0CC2o7MwS36ZEismjU5gIey7rntdrrf2K3LaTXv_Fdh84Zv73SMEStmB7AEQlemd0mOh0Jo7nYHEE8bp-9npuUVtk3USt6CxIQDGBAWuDlEn6Qcbnqf7t4EWEyKK7wXv8zcJ9mTcp7b-JDYkTE17XQ8BLSFG1h_dYKLzZBa5MPhiwUTW3UdL_BL0voEXnwPhe-jNwYFPUbtE2MRZAMaZ5Gx94nhBwXID7obRaG0M3btRHkhCm-lrA0qmkRP3d_dpjtPDSDiHje_R5mBP4LQpBwiXv=s167-p-k-no", text:"Women's Day Event", date:"March 9' 2017", cta:"https://goo.gl/photos/sTgVqSFSJxDZGvH77"};
-pics[1]= {url:"https://lh3.googleusercontent.com/2Ez1kAfDIHsmPHSwTaUj8kNFyqyJTb2ZXY8UWdaUsAyGnXVRAiNNOXmiRl1-um5ro1-d2v9MGzZIktffAZcs7-EPJ5j8iHD5vsq0qGVppppSUhcQBiH2q2GDvjXV8fXMsODTw0A-y3COGqa4n8JYjDbAdGupiBWe9IOC7-aMhK71Sw8Ol6jdPEdZ2qadLJ6nyq0x-WMXGSS-b9G_I5nwohu_fLq1Kn0-IYaEV0LFOtP70kIMmTXtmSbVvRCEXX7MVTkTBnYM70DUDAwy0_wrO6i_LqjgqvasDDh1WrXJaWbecLu1XEk489dmHCAH5vaphK9FPrF_vpDuKyBcN4fmKjrYZwBiJOQllgs5SyvWCQugxs5SnDR7F2ycAg8ETqIh2_K_4lH07krqAYLzDuq67m5CTVPa1HnUqbnqeDn-S1shYXpjnawW8WPUhCMZtrL3Tx4uvjmC_pGy2OHYqlsfltYCHQ0RDZzQdRVQ1yaZnkdPsNGCKMBjfiPAIGn3o1nBtQsuu6IllQwwlV57jlpkvg30nb7l0M767yTU2sradqD7Q1VEN0_gfD2xY3p6eYZTGw0CBpv0nRTNCxzmUctjFaaYJ45SQfEcZqDJWWGikPHp-70b=s190-p-k-no", text:"Ethical Hacking", date:"March 2' 2017", cta:"https://goo.gl/photos/uoBWC4foibRyzxd58"};
-pics[2]= {url:"https://lh3.googleusercontent.com/JY1Xlzfyzsc8rhWDb8pFfrCTyJtdqkxMGqnX0xJfagYYrFlHRwrwgGjV_4oiLs4MQlHe21nhovTEixP3OjbIm__xTSAPpHoYBsR3K_WTrsoTpyjhsO0OOiRwPZj-Eec5cwW63e5NM28D4OPjvg7L-tb4uEYhxnsNgisABx0GTcDKgpIdYkmtUThjXP33NlqVYaqDmrqfdSYyTm_lItDu97ue4gtb8pLSVkWySFMlFKXACp8ByGaTkLfucExEaQbPsO99gbQ6nSVgw8Tlv5slRfxxd7giqjyJldYHS_3pWcpl68UeuW7CqYE--8Jl9EK8ku0OwBlXXpHMAGscg2nki6rfjV1V8IK22vTkOGfki3_mE-DigkyhePw5bQzawbUyqEBy0sZzv6obdN1MSiYay1tKJOTKqUnZYqxwtZWcPwLIIJT1o_Lk92_49kKXLbFTJ6q29dWlXDwOqNOdmL-Na2D2IPeaneCxZy-K6EQA0JfvysLMvddJLdRBi7jgi0YydvKwDJFkiJ7TB3ZiQaW4ZrHCgKbuqkmKDVv57CJytqkoZb2xWaB5knW7gAxSZuQ9SHcAgpUyokhTvL_HPJD5VfeBDLWTzrzaLLIoJ_jPienqvGng=s170-p-k-no", text:"Amity Youth Fest Meetup", date:"February 18' 2017", cta:"https://goo.gl/photos/HVBzRy8Jw1fs7Yyc7"};
-pics[3]= {url:"https://lh3.googleusercontent.com/qu04uXsdaq9jBzRPzzqZRStGZt42KbjTjJGaJdmoeBPq-AfoplY-grfZljZY984hAr7pNd3YC2Kr8fZfIO9_aBGEgr5HyZYNK1lPHNTtvRzfGONL0ko8Nk8eEq11cYTm1CJ3IxrnlQXonu2CQGiJO7ZdhLW1vscfMuur1C4S6SpUPH6hQuWV0zE-jrpcOHb3lulKPZjAkj5QofUD0KkT5-IT5iBg7qJem5AlZTE1TfR5QUGhsabt8-9j7Kb40tl6TsObzk6X3OsQ72Pph_J8sXkz75AF2HnIeHrRb655QT3A--lMF5CmxKaGxsrTk2ELLDPBZPLQFl7HiVZi6qi8j6rYlDRRbEfmrNbj_Bp-bd3lNG8RFhLgURIUS_wuQfUNyguMfQ52ru19c27alcwD4h04p0iUdVMc7Ykq66ngheBUS9uV-AWwdm2Fuk9u3J3s5D0pa_tQh1cj_YsEdT9K7l8ln6NsnB2nnEtBQ8kkYZMtVBHs5B-pp30-7AibFxhj4B8pozmb5uhE4BI2l6Y0y9W6gQIaomJYCyGQRday8jKt0nUD7wQf70HszrenSku9sQI2QwCCv4YZOfp8T8DGXynyNu491nZR2Kh3GuhizWdE0--5=s169-p-k-no", text:"Operating Systems", date:"February 7' 2017", cta:"https://goo.gl/photos/9D8csT4v9RsnrVzm7"};
 
-/** Resources Box List
-	cta: Call to action: Link to Document
-	url: Link to picture displayed
-	text: text to appear at mouse hover.
-**/
-resource[0]={cta:"https://docs.google.com/document/d/1s3RWKEemXNnCOPb_YyEMFIjC5C6R8R8K2gxMXRQyG7U/edit?usp=sharing",url:"images/Resources/OpnSrcGrps.jpg", text:"Links to Opensource Communities in Delhi/NCR."};
-resource[1]={cta:"https://docs.google.com/document/d/13h5MoQZv4KgnLjkeDiRHjaAZRF-VaUpy-ZN4EltIwiA/edit?usp=sharing",url:"images/Resources/StudyMaterial.jpg", text:"Links to helpful Study Material & Resources."};
-resource[2]={cta:"https://docs.google.com/document/d/1BVU3XOpvL8Gb1Px1VlI_NU018Hoj06aOfp69_NgERjo/edit?usp=sharing",url:"images/Resources/RecCompt.png", text:"Links to events recommended for participation."};
- 
+/** Open Source Communities List
+  
+ */
+openSource[0] = {url:"https://pydelhi.org", logo:"images/logos/pydelhi.png", title:"PyDelhi"};
+openSource[1] = {url:"https://ilugd.github.io/", logo:"images/logos/ilugd.png", title:"Indian Linux Users Group Delhi"};
+openSource[2] = {url:"http://linuxchixindia.github.io", logo:"images/logos/linuxchix.png", title:"LinuxChix India"};
+openSource[3] = {url:"http://mozpacers.org/", logo:"images/logos/mozpacers.png", title:"Mozilla Delhi"};
+openSource[4] = {url:"https://www.facebook.com/groups/0p3nS0urc3M33tups/", logo:"images/logos/opensource.png", title:"Open Source Community"};
+openSource[5] = {url:"https://in.pycon.org", logo:"images/logos/pycon.png", title:"PyCon India"};
+
+/*	Participate & Learn
+*/
+
+eventsP[0] = {url:"https://summerofcode.withgoogle.com/", logo:"images/logos/gsoc.png", title:"Google Summer of Code"};
+eventsP[1] = {url:"https://ilugd.github.io/", logo:"images/logos/outreachy.png", title:"Outreachy"};
+eventsP[2] = {url:"https://in.pycon.org", logo:"images/logos/pycon.png", title:"PyCon India"};
+
 /** Team List 
 	team[0] = {name:"Test Name 1", designation: "Events Head", ppicUrl: "images/alumni/1.jpg"};
 	name: Full Name
-	designation: designation
+	designation: FIll your github username
 	ppicUrl: Realtive url to images/team/name.jpg
 **/
-team[0] = {name:"Shivam Rajput", designation: "", ppicUrl: "images/team/shivam.jpg"};
-team[1] = {name:"Ashish Dahiya", designation: "", ppicUrl: "images/team/ashish.jpg"};
-team[2] = {name:"Tanya Jain", designation: "", ppicUrl: "images/team/tanya.jpg"};
-team[4] = {name:"Ayush Agarwal", designation: "", ppicUrl: "images/team/ayush.jpg"};
-team[5] = {name:"Parth Sharma", designation: "", ppicUrl: "images/team/parth.jpg"};
-team[6] = {name:"Animesh Verma", designation: "", ppicUrl: "images/team/animesh.jpg"};
-team[7] = {name:"Omkar Yadav", designation: "", ppicUrl: "images/team/omkar.jpg"};
+team[0] = {name:"Shivam Rajput", designation: "#", ppicUrl: "images/team/shivam.jpg"};
+team[1] = {name:"Ashish Dahiya", designation: "#", ppicUrl: "images/team/ashish.jpg"};
+team[2] = {name:"Tanya Jain", designation: "Tanya-Jain", ppicUrl: "images/team/tanya.jpg"};
+team[3] = {name:"Ayush Agarwal", designation: "thisisayush", ppicUrl: "images/team/ayush.jpg"};
+team[4] = {name:"Parth Sharma", designation: "parthsharma2", ppicUrl: "images/team/parth.jpg"};
+team[5] = {name:"Animesh Verma", designation: "AnimeshVerma17", ppicUrl: "images/team/animesh.jpg"};
+team[6] = {name:"Omkar Yadav", designation: "omkar108", ppicUrl: "images/team/omkar.jpg"};
 /** End Team List **/
 
 /** Footer Contact Links 
@@ -132,11 +128,10 @@ var siteServicesListContainer = $(".servicesList");
 var eventsListContainer = $("#eventsListContainer");
 var webinarsListContainer = $("#webinarsListContainer");
 var alumniDescriptionContainer = $(".alumniDescription p");
+var openSourceContainer = $("#openSourceListContainer");
+var eventsPListContainer = $("#eventsParticipateListContainer");	
 var teamListContainer = $(".teamList");
-var pastEventPicsContainer = $("#pastEventPicsContainer");
-var rankersListContainer = $("#RankersListContainer");
-var resourceListContainer = $("#resourceListContainer");
-var footerContactLinksContainer = $("#contactLinksContainer");
+var footerContactLinksContainer = $("#contactLinksContainer-1, #contactLinksContainer-2");
 /**Flashbox Containers */
 var flashBox = $(".flashbox");
 var flashBoxContentContainer = $(".flashBox-content");
--- js/main.js
+++ js/main.js
@@ -12,6 +12,69 @@ $(document).ready(function(e){
     /** Generate Boxes */
     generateAllBoxes();
     generateContactLinks();
-    setTheme("blue","deep-orange");
+    //setTheme("blue","deep-orange");
     $(".preloader").delay(1000).fadeOut(2000);  //hide preloader
+    responsive();
+    $(".single-item").slick({
+        dots: false,
+        autoplay:true,
+        arrows: true,
+        infinite: true 
+    });
+    $("#webinarsListContainer, #eventsListContainer").slick({
+        dots: true,
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    $(".teamList").slick({
+        dots: true,
+        arrows: true,
+        centerMode: true,
+        centerPadding: '40px',
+        mobileFirst: true,
+        autoplay: true,
+        autoplaySpeed:1000,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
